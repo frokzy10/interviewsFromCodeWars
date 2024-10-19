5f6fce2af6d043f28bd7e1cede03176340246d0c
Нурдин Бакытбеков <nurdinbakytbekov@MacBook-Air-Nurdin.local>
2024-10-19 12:19:44 +0600

    update quicksort and validateQuotos

--- src/QuickSort/QuickSort.java
+++ src/QuickSort/QuickSort.java
@@ -1,2 +1,39 @@
-package QuickSort;public class QuickSort {
+package QuickSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickSort {
+    private static int iterationCount;
+    public static void main(String[] args) {
+        List<Integer> list = List.of(10, 7, 8, 9, 1, 5);
+        System.out.println("Исходный массив: " + list);
+        List<Integer> sortedArr = qsort(list);
+        System.out.println("Отсортированный массив: " + sortedArr);
+    }
+    private static List<Integer> qsort(List<Integer> arr){
+        if(arr.size() < 2){
+            return arr;
+        }else {
+            int pivot = arr.getFirst();
+
+            List<Integer> less = new ArrayList<>();
+            List<Integer> more = new ArrayList<>();
+
+            for (int i = 1 ; i < arr.size(); i++){
+                if(arr.get(i) < pivot){
+                    less.add(arr.get(i));
+                }else{
+                    more.add(arr.get(i));
+                }
+            }
+            System.out.println(iterationCount++);
+            List<Integer> result = new ArrayList<>();
+            result.addAll(qsort(less));
+            result.add(pivot);
+            result.addAll(qsort(more));
+
+            return result;
+        }
+    }
 }
--- src/validateQuotos/validateQuotos.java
+++ src/validateQuotos/validateQuotos.java
@@ -1,2 +1,28 @@
-package validateQuotos;public class validateQuotos {
+package validateQuotos;
+
+import java.util.Stack;
+
+public class validateQuotos {
+    public static void main(String[] args) {
+        String res = "())";
+        System.out.println(validate(res));
+    }
+    private static boolean validate(String output){
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : output.toCharArray()){
+            if(c == '('){
+                stack.push(c);
+            }else if(c == ')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
+
+// ()(()()
